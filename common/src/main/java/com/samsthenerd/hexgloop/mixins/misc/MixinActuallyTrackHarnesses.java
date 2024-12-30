187a7df70beeca6b4680d94f96f30cbd6ea031d9
JordanPlayz158 <JordanAdamsBusiness@outlook.com>
2024-12-29 22:31:53 -0500

    Fixed signature of MixinActuallyTrackHarnesses

--- common/src/main/java/com/samsthenerd/hexgloop/mixins/misc/MixinActuallyTrackHarnesses.java
+++ common/src/main/java/com/samsthenerd/hexgloop/mixins/misc/MixinActuallyTrackHarnesses.java
@@ -1,5 +1,6 @@
 package com.samsthenerd.hexgloop.mixins.misc;
 
+import at.petrak.hexcasting.api.casting.eval.ExecutionClientView;
 import at.petrak.hexcasting.api.casting.eval.vm.CastingVM;
 import java.util.List;
 
@@ -11,7 +12,6 @@ import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import com.samsthenerd.hexgloop.casting.IContextHelper;
 import com.samsthenerd.hexgloop.casting.gloopifact.ICADHarnessStorage;
 
-import at.petrak.hexcasting.api.spell.casting.ControllerInfo;
 import at.petrak.hexcasting.api.casting.iota.Iota;
 import at.petrak.hexcasting.common.items.magic.ItemPackagedHex;
 import net.minecraft.item.ItemStack;
@@ -20,20 +20,22 @@ import net.minecraft.server.world.ServerWorld;
 
 @Mixin(ItemPackagedHex.class)
 public class MixinActuallyTrackHarnesses {
-    @WrapOperation(method="use(Lnet/minecraft/world/World;Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/Hand;)Lnet/minecraft/util/TypedActionResult;",
-    at=@At(value="INVOKE", target="at/petrak/hexcasting/api/spell/casting/CastingVM.executeIotas (Ljava/util/List;Lnet/minecraft/server/world/ServerWorld;)Lat/petrak/hexcasting/api/spell/casting/ControllerInfo;"))
-    public ControllerInfo trackHarnesses(CastingVM harness, List<Iota> instrs, ServerWorld sWorld, Operation<ControllerInfo> original){
-        ServerPlayerEntity sPlayer = (ServerPlayerEntity) harness.getEnv().getCastingEntity();
-        ItemStack castingStackProbably = sPlayer.getStackInHand(harness.getEnv().getCastingHand());
-        ((IContextHelper)(Object)harness.getEnv()).setCastingItem(castingStackProbably);
+    @WrapOperation(method="use",
+        at=@At(value="INVOKE", target="Lat/petrak/hexcasting/api/casting/eval/vm/CastingVM;queueExecuteAndWrapIotas(Ljava/util/List;Lnet/minecraft/server/world/ServerWorld;)Lat/petrak/hexcasting/api/casting/eval/ExecutionClientView;"))
+    public ExecutionClientView trackHarnesses(CastingVM vm, List<? extends Iota> instrs,
+        ServerWorld sWorld, Operation<ExecutionClientView> original){
+
+        ServerPlayerEntity sPlayer = (ServerPlayerEntity) vm.getEnv().getCastingEntity();
+        ItemStack castingStackProbably = sPlayer.getStackInHand(vm.getEnv().getCastingHand());
+        ((IContextHelper)(Object)vm.getEnv()).setCastingItem(castingStackProbably);
         if(sPlayer != null){
             ICADHarnessStorage storage = (ICADHarnessStorage)(Object)sPlayer;
-            storage.addHarness(harness);
+            storage.addHarness(vm);
         }
-        ControllerInfo info = original.call(harness, instrs, sWorld);
+        ExecutionClientView info = original.call(vm, instrs, sWorld);
         if(sPlayer != null){
             ICADHarnessStorage storage = (ICADHarnessStorage)(Object)sPlayer;
-            storage.removeHarness(harness);
+            storage.removeHarness(vm);
         }
         return info;
     }
